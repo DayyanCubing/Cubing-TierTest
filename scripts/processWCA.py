7011c31d20146a75e72249a30f73eb356f62ce30
DayyanCubing <dayyan.fahad.mirza@gmail.com>
2026-08-08 19:24:54 +0100

    Finalised Data Processing

--- scripts/processWCA.py
+++ scripts/processWCA.py
@@ -1,7 +1,9 @@
 import requests
 import zipfile
 import csv
-'''
+import math
+import json
+
 url = "https://www.worldcubeassociation.org/api/v0/export/public"
 
 response = requests.get(url)
@@ -14,7 +16,7 @@ tsv_response = requests.get(tsv_url)
 with open("wca_export.zip", "wb") as f:
     f.write(tsv_response.content)
 
-'''
+
 # Store all competitors' best averages, grouped by event
 event_times = {}
 
@@ -41,8 +43,24 @@ with zipfile.ZipFile("wca_export.zip", "r") as zip_file:
 
 competitors = {}
 
-events = ["222","333","444","555","666","777","333oh","minx","pyram","clock","sq1","skewb","333bf","444bf","555bf"] #It is missing FTO
+events = ["222","333","444","555","666","777","333oh","minx","pyram","clock","sq1","skewb","333bf","444bf","555bf"]#It is missing FTO
+
+tiers = {"F": 0, "E":20, "D":40, "C":70, "B":90, "A":99, "A+":99.9, "S":99.99, "S+":99.999, "SS": 99.9999}
+
 for event in events:
     competitors[event] = len(event_times[event])
 
-print(competitors)
+cutoffs = {
+    event: {
+        tier: event_times[event][
+            math.ceil(
+                competitors[event] * (1 - tiers[tier] / 100)
+            ) - 1
+        ]
+        for tier in tiers
+    }
+    for event in events
+}
+
+with open("data/cutoffs.json", "w") as file:
+    json.dump(cutoffs, file, indent=4)
