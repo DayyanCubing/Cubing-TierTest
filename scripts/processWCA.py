eda7df15f48c9153d7c5da3ec03bd2e374bbe2d3
DayyanCubing <dayyan.fahad.mirza@gmail.com>
2026-08-08 14:48:55 +0100

    Worked on data processing

--- scripts/processWCA.py
+++ scripts/processWCA.py
@@ -1,4 +1,5 @@
 import requests
+import zipfile
 
 url = "https://www.worldcubeassociation.org/api/v0/export/public"
 
@@ -13,4 +14,10 @@ tsv_response = requests.get(tsv_url)
 with open("wca_export.zip", "wb") as f:
     f.write(tsv_response.content)
 
-print("Download complete")
+
+
+
+with zipfile.ZipFile("wca_export.zip", "r") as zip_file:
+    with zip_file.open("WCA_export_ranks_average.tsv") as file:
+        for i in range(5):
+            print(file.readline().decode("utf-8").rstrip())
